d057706e01ecbe254af2ac102cb06272a954811f
Ibro <ibroabdraimov20@gmail.com>
2025-07-01 22:35:29 +0600

    Ib:

--- coaches/urls.py
+++ coaches/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.coaches_list, name='coaches-list'),  # это HTML
-    path('api/', views.coaches_api, name='coaches-api'),  # это JSON
+    path('', views.coaches_api, name='coaches-api'),
+    path('api/', views.coaches_api, name='coaches-api'),  # JSON
 ]
